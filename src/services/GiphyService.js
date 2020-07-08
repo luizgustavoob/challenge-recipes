9c80ce3b589a326c5d9b41d44be1894657c405e9
Luiz Gustavo Bourscheit <oluizgustavob@gmail.com>
2020-07-08 14:04:53 -0300

    fix: handle url giphy

--- src/services/GiphyService.js
+++ src/services/GiphyService.js
@@ -9,11 +9,13 @@ module.exports = {
       const response = await axios(BASE_URL, {
         params: {
           api_key: process.env.GIPHY_API_KEY,
-          q: title
+          q: title,
+          limit: 1
         }
       });
 
-      return response.data.data[0].images.downsized_large.url;
+      const urlFull = response.data.data[0].images.downsized_large.url;      
+      return urlFull.substring(0, urlFull.indexOf('?'));
     } catch(err) {
       throw new Error(err);
     }
